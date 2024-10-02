6f459b745dd0b90032331cb8a085ecba83181e3e
francolloveras <francorlloveras@gmail.com>
2024-10-01 21:41:51 -0300

    Change title color and Next.js name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex items-center flex-col gap-y-4">
       <header className="pt-8 pb-2">
-        <h1 className="text-3xl font-semibold text-neutral-100">NextJS with Recharts</h1>
+        <h1 className="text-3xl font-semibold text-white">Next.js with Recharts</h1>
       </header>
       <section className="flex gap-x-4">
         <DailyCard title="Sales today" range={0} />
