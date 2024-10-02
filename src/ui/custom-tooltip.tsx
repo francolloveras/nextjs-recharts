da32a6252f9194f18500d18c2444e716fc2b4402
francolloveras <francorlloveras@gmail.com>
2024-10-02 19:56:53 -0300

    Fix custom tooltip pie chart item color error

--- src/ui/custom-tooltip.tsx
+++ src/ui/custom-tooltip.tsx
@@ -10,9 +10,14 @@ export default function CustomTooltip({ active, payload, label }: Props) {
   return (
     <ul className="tooltip-container">
       {typeof label === 'string' && label && <p>{label}:</p>}
-      {payload.map(({ dataKey, name, value, color }) => (
+      {payload.map(({ dataKey, name, value, color, payload }) => (
         <li key={dataKey} className="tooltip-item">
-          {color && <span className="tooltip-block-color" style={{ backgroundColor: color }} />}
+          {(payload.color || color) && (
+            <span
+              className="tooltip-block-color"
+              style={{ backgroundColor: payload.color ?? color }}
+            />
+          )}
           {name}: {value}
         </li>
       ))}
