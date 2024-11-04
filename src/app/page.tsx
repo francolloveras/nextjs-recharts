b15e806964f2dc57fd83ff2b1a2f537ef52b90e8
francolloveras <francorlloveras@gmail.com>
2024-11-03 21:54:47 -0300

    Add Daily Card timeout to create a fake timeout request and add the skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,26 @@
+import { Suspense } from 'react'
+
 import DailyCard from '@/components/daily-card'
 import Footer from '@/components/footer'
 import MonthCard from '@/components/month-card'
+import DailyCardSkeleton from '@/ui/daily-card-skeleton'
 
 export default function Home() {
   return (
-    <main className="flex max-w-7xl mx-auto flex-col gap-y-4">
-      <header className="pt-8 text-center pb-2">
+    <main className="mx-auto flex max-w-7xl flex-col gap-y-4">
+      <header className="pb-2 pt-8 text-center">
         <h1 className="text-3xl font-semibold text-white">Next.js with Recharts</h1>
       </header>
       <section className="flex gap-x-4">
-        <DailyCard title="Sales today" range={0} />
-        <DailyCard title="Sales 7 days" range={7} />
-        <DailyCard title="Sales 30 days" range={30} />
+        <Suspense fallback={<DailyCardSkeleton />}>
+          <DailyCard title="Sales today" range={0} />
+        </Suspense>
+        <Suspense fallback={<DailyCardSkeleton />}>
+          <DailyCard title="Sales 7 days" range={7} timeout={2000} />
+        </Suspense>
+        <Suspense fallback={<DailyCardSkeleton />}>
+          <DailyCard title="Sales 30 days" range={30} timeout={4000} />
+        </Suspense>
       </section>
       <section className="h-72 w-full rounded-md border border-neutral-600 bg-neutral-900 px-6 py-4">
         <MonthCard />
