7bb0a9c21d8ee09860d9ee5a246447c9e97abe74
Franco Lloveras <77513994+francolloveras@users.noreply.github.com>
2026-01-25 15:53:09 -0300

    Add base path route in next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/form-carousel',
+  assetPrefix: '/form-carousel/'
+};
 
 export default nextConfig;
